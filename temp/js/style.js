2362a639b333e05cee4fbb2d4b9c941e3c841947
munther alassar <107102775+montheralassar@users.noreply.github.com>
2022-12-23 11:33:48 +0200

    port folio
    i use html css javascript

---
+++ temp/js/style.js
@@ -0,0 +1,12 @@
+let ButtonNanMobil = document.querySelector('#icon-nav-bar');
+
+let NavBarMobil = document.querySelector('.nav-bar-mobil');
+
+
+
+ButtonNanMobil.onclick = _ => {
+    NavBarMobil.classList.toggle("show");
+
+    console.log(NavBarMobil);
+
+}
